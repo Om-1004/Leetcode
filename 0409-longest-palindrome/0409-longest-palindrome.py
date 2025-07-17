43acd33036d3c0577a272460411dd02cb9686d0c
Om-1004 <96159322+Om-1004@users.noreply.github.com>
2025-07-17 18:25:09 -0400

    Time: 0 ms (100%), Space: 17.9 MB (45.98%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,26 +1,22 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        obj = {}
-        if len(s) == 1:
-            return 1
-        for i in range(len(s)):
-            if s[i] not in obj:
-                obj[s[i]] = 1
-            else:
-                obj[s[i]] += 1
-        
-        length = 0
-        hasOddValue = False
+        dictt = dict()
 
-        for j in obj.values():
-            if j % 2 != 0:
-                hasOddValue = True
-                length = length + j - 1
-            else:
-                length += j
+        for letter in s:
+            dictt[letter] = 1 + dictt.get(letter, 0)
         
-        if hasOddValue == True:
-            length += 1
+        count = 0
+        flag = False
 
-        return length
-        
+        for value in dictt.values():
+            if value % 2 == 0:
+                count += value
+            else:
+                counter = value // 2
+                count += (counter * 2)
+                flag = True
+
+        return count + 1 if flag else count
+
+
+            
